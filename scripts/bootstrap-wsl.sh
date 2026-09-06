54fddfff4bf09bcc11b35343a09161378fba3a67
Petr <petr.vasilyev.1996@ya.ru>
2026-09-06 22:08:49 +0300

    docs: archive initial service specification

--- scripts/bootstrap-wsl.sh
+++ scripts/bootstrap-wsl.sh
@@ -41,7 +41,33 @@ install_uv_and_python() {
 
 require_root
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y ca-certificates curl git make
+DEBIAN_FRONTEND=noninteractive apt-get install -y \
+  ca-certificates \
+  curl \
+  git \
+  make \
+  libasound2t64 \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libatspi2.0-0 \
+  libcairo2 \
+  libcups2t64 \
+  libdbus-1-3 \
+  libdrm2 \
+  libgbm1 \
+  libglib2.0-0 \
+  libnspr4 \
+  libnss3 \
+  libpango-1.0-0 \
+  libwayland-client0 \
+  libx11-6 \
+  libxcb1 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxext6 \
+  libxfixes3 \
+  libxkbcommon0 \
+  libxrandr2
 configure_docker_repository
 configure_node_repository
 apt-get update
